b9612a391fd99251c883424b210e2825208551f7
Александр Ляпко <lyapko@vbankcenter.ru>
2025-08-28 14:02:04 +0400

    Decouple Dockerfile from main site

--- start.sh
+++ start.sh
@@ -6,12 +6,10 @@ export CRON_IP=`dig +short cron-service`
 
 # envsubst < ./kernel/config.local.template > kernel/config.local.php
 envsubst '$${HOST_NAME}' < ./conf/observer.template > /etc/apache2/sites-available/observer.conf
-cp -f ./conf/site.template /etc/apache2/sites-available/site.conf
 
 case $SRV_RUN in
   WWW )
     a2ensite observer
-    a2ensite site
     apache2ctl -D FOREGROUND
     ;;
   JOB )
